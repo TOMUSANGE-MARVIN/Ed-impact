81a2dfbb07871d022399c25074b20df0eaf86f0b
TOMUSANGE-MARVIN <marvintomusange@gmail.com>
2026-09-11 18:04:54 +0300

    Replace About page gallery placeholders with Unsplash photography
    The gallery strip's six images were still the original gradient
    placeholders. Replace with real classroom photography (Unsplash, free
    license) and refactor the component to map over an array with proper
    alt text instead of 12 duplicated blocks with generic alt="Image_inner".

--- src/components/GalleryOne.jsx
+++ src/components/GalleryOne.jsx
@@ -1,82 +1,27 @@
 import Link from "next/link";
 
+const images = [
+  { file: "one.png", alt: "Students seated at desks during a lesson" },
+  { file: "two.png", alt: "Classroom of learners in Uganda" },
+  { file: "three.png", alt: "A learner focused on classwork" },
+  { file: "four.png", alt: "A learner smiling at the chalkboard" },
+  { file: "five.png", alt: "Learners writing in their notebooks" },
+  { file: "six.png", alt: "Learners working together at their desks" },
+];
+
 const GalleryOne = () => {
   return (
     <div className='gallery'>
       <div className='gallery__inner'>
         <div className='gallery__slider'>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/one.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/two.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/three.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/four.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/five.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/six.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/one.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/two.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/three.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/four.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/five.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
-          <div className='gallery__single'>
-            <img src='assets/images/gallery/six.png' alt='Image_inner' />
-            <Link href='/'>
-              <i className='fa-brands fa-instagram' />
-            </Link>
-          </div>
+          {[...images, ...images].map((image, index) => (
+            <div className='gallery__single' key={index}>
+              <img src={`/assets/images/gallery/${image.file}`} alt={image.alt} />
+              <Link href='/'>
+                <i className='fa-brands fa-instagram' />
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
